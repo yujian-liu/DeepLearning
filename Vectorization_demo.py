0438c817919472c561b054cab41b4b625909c81f
yujian-liu <18869950811@139.com>
2025-09-02 21:09:01 +0800

    add code comment and judge
    向量化的运行结果和非向量化的结果不一致，判断后为浮点数计算导致的偏差

--- Vectorization_demo.py
+++ Vectorization_demo.py
@@ -7,18 +7,27 @@ print(a)
 a = np.random.rand(1000000)
 b = np.random.rand(1000000)
 
+# 向量化
 tic = time.time()
-c = np.dot(a,b)
+c1 = np.dot(a,b)
 toc = time.time()
 
-print(c)
+print(c1)
 print("Vectorized version:" + str(1000 * (toc - tic)) + "ms")
 
-c = 0
+# 非向量化
+c2 = 0
 tic = time.time()
 for i in range(1000000):
-    c += a[i] * b[i]
+    c2 += a[i] * b[i]
 toc = time.time()
 
-print(c)
+print(c2)
 print("For loop:" + str(1000 * (toc - tic)) + "ms")
+
+# 发现向量化与非向量化的结果不一致，进行进一步判断
+# 应该是由于浮点数计算导致的误差
+if np.allclose(c1, c2, atol=1e-10):
+    print("结果在精度范围内一致 ✅")
+else:
+    print("结果差异较大 ❌")
